5a526996a75112ab7692e10d95ed66395eebedd3
ssaeed4 <60822755+ssaeed4@users.noreply.github.com>
2023-10-24 23:59:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 const express = require('express');
 const app = express();
-
-// Define route for addition
-app.get('/add/:num1/:num2', (req, res) => {
-  const num1 = parseFloat(req.params.num1);
-  const num2 = parseFloat(req.params.num2);
-  const result = num1 + num2;
-  res.json({ operation: 'addition', result });
-});
-
-// Define route for subtraction
-app.get('/subtract/:num1/:num2', (req, res) => {
-  const num1 = parseFloat(req.params.num1);
-  const num2 = parseFloat(req.params.num2);
-  const result = num1 - num2;
-  res.json({ operation: 'subtraction', result });
-});
-
-// Define route for multiplication
-app.get('/multiply/:num1/:num2', (req, res) => {
-  const num1 = parseFloat(req.params.num1);
-  const num2 = parseFloat(req.params.num2);
-  const result = num1 * num2;
-  res.json({ operation: 'multiplication', result });
-});
-
-// Define route for division
-app.get('/divide/:num1/:num2', (req, res) => {
-  const num1 = parseFloat(req.params.num1);
-  const num2 = parseFloat(req.params.num2);
-  if (num2 === 0) {
-    res.status(400).json({ error: 'Division by zero is not allowed' });
-  } else {
-    const result = num1 / num2;
-    res.json({ operation: 'division', result });
-  }
-});
-
 const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', { result: '' });
+});
+
+app.post('/calculate', (req, res) => {
+    const num1 = parseFloat(req.body.num1);
+    const num2 = parseFloat(req.body.num2);
+    const operation = req.body.operation;
+    let result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 === 0) {
+                result = 'Division by zero is not allowed';
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            result = 'Invalid operation';
+    }
+
+    res.render('index', { result });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
